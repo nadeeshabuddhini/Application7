d41b96acc5692b4124dbf1aa366217363d968ffd
Nadeesha Buddhini <hknadeesha1995buddhi@gmail.com>
2019-03-05 22:22:02 +0530

    Application7
    Application7

---
+++ src/application7/Application7.java
@@ -0,0 +1,16 @@
+
+package application7;
+
+
+public class Application7 {
+
+    public static void main(String[] args) {
+       int i,j,n=5;
+       for(i=1;i<=n;i++){
+           for(j=1;j<=i;j++)
+               System.out.print(i);
+           System.out.println("");
+       }
+    }
+    
+}
